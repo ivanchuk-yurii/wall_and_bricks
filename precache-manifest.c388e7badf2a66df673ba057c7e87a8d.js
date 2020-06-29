650fe7c1475c1dc9b9794d75433be12b2e1e2c8e
yura <wa7er14@gmail.com>
2020-06-29 22:24:56 +0300

    Updates

---
+++ precache-manifest.c388e7badf2a66df673ba057c7e87a8d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcb6b8894a8626a39fcb0b45e6fcc6fd",
+    "url": "/wall_and_bricks/index.html"
+  },
+  {
+    "revision": "013239b6ae13213e094c",
+    "url": "/wall_and_bricks/static/css/main.a5263faf.chunk.css"
+  },
+  {
+    "revision": "48fbf44acdac92ed2fe9",
+    "url": "/wall_and_bricks/static/js/2.41beef36.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/wall_and_bricks/static/js/2.41beef36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "013239b6ae13213e094c",
+    "url": "/wall_and_bricks/static/js/main.9756630b.chunk.js"
+  },
+  {
+    "revision": "03adc493edc61deeafc2",
+    "url": "/wall_and_bricks/static/js/runtime-main.6afc1809.js"
+  }
+]);
